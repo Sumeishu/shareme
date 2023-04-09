0a5115df1d853c4ce0b595a19ecf650e4a2245dd
Sumeishu <1439234574@qq.com>
2023-04-09 17:27:30 +0800

    a bug in this project, i already given up it. this is final commit

--- shareme_frontend/src/client.js
+++ shareme_frontend/src/client.js
@@ -1,7 +1,7 @@
-import {createClient} from '@sanity/client'
 import {imageUrlBuilder} from '@sanity/image-url'
+import {createClient} from '@sanity/client'
 
-export const client = createClient({
+export const client =  createClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2023-04-07',
